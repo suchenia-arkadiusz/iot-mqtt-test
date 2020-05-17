0692cbdcb5571a3a3ff450c0f91053dd8daed952
Arkadiusz Suchenia <suchenia.arkadiusz@aru-software.pl>
2020-05-17 13:28:41 +0200

    Added read data from sensor

--- mqtt-test-device/src/main.cpp
+++ mqtt-test-device/src/main.cpp
@@ -1,9 +1,37 @@
-#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_BME280.h>
+#include <Adafruit_Sensor.h>
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+#define SERIAL_NUMBER "NMCU_1589714856299"
+
+Adafruit_BME280 bme;
+
+float temperature, humidity, pressure, altitude;
+
+void readData() {
+  Serial.println("------------------------------read data----------------------------");
+  temperature = bme.readTemperature();
+  humidity = bme.readHumidity();
+  pressure = bme.readPressure() / 100.0F;
+  altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(100);
+
+  bme.begin(0x76);
+
+  Serial.println();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  readData();
+
+  Serial.println("temp = " + String(temperature));
+  Serial.println("hum = " + String(humidity));
+  Serial.println("pres = " + String(pressure));
+  Serial.println("alt = " + String(altitude));
+  delay(10000);
 }
