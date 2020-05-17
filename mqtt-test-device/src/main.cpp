f8087677307441f2b77a890ab67675881c635425
Arkadiusz Suchenia <suchenia.arkadiusz@aru-software.pl>
2020-05-17 14:00:45 +0200

    Added connection with wifi

--- mqtt-test-device/src/main.cpp
+++ mqtt-test-device/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Wire.h>
 #include <Adafruit_BME280.h>
 #include <Adafruit_Sensor.h>
+#include <ESP8266WiFi.h>
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 #define SERIAL_NUMBER "NMCU_1589714856299"
@@ -17,13 +18,32 @@ void readData() {
   altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
 }
 
+void configureWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.hostname(SERIAL_NUMBER);
+  WiFi.begin("dom");
+
+  Serial.print("Connecting with wifi");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected!");
+
+  Serial.print(SERIAL_NUMBER);
+  Serial.println("'s IP Address = " + WiFi.localIP().toString());
+}
+
 void setup() {
   Serial.begin(115200);
   delay(100);
 
   bme.begin(0x76);
+  delay(100);
 
   Serial.println();
+
+  configureWiFi();
 }
 
 void loop() {
