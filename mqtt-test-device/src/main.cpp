188b5d053ff126bba87b4801cbee49cca071e9a7
Arkadiusz Suchenia <suchenia.arkadiusz@aru-software.pl>
2020-06-14 13:13:58 +0200

    GH-1 - Send data from the device via the MQTT protocol

--- mqtt-test-device/src/main.cpp
+++ mqtt-test-device/src/main.cpp
@@ -2,13 +2,20 @@
 #include <Adafruit_BME280.h>
 #include <Adafruit_Sensor.h>
 #include <ESP8266WiFi.h>
+#include <MQTT.h>
+#include <ArduinoJson.h>
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 #define SERIAL_NUMBER "NMCU_1589714856299"
 
 Adafruit_BME280 bme;
+WiFiClient net;
+MQTTClient client;
+
+String publishTopic = "adevice/" + String(SERIAL_NUMBER) + "/data";
 
 float temperature, humidity, pressure, altitude;
+String json;
 
 void readData() {
   Serial.println("------------------------------read data----------------------------");
@@ -16,6 +23,30 @@ void readData() {
   humidity = bme.readHumidity();
   pressure = bme.readPressure() / 100.0F;
   altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+}
+
+void generateJson() {
+  StaticJsonDocument<200> object;
+  object["temperature"] = temperature;
+  object["humidity"] = humidity;
+  object["pressure"] = pressure;
+  object["altitude"] = altitude;
+
+  json = "";
+  serializeJson(object, json);
+}
+
+void sendMessage() {
+  Serial.println("Publish message: " + json);
+  client.publish(publishTopic.c_str(), json.c_str());
+}
+
+void reconnect() {
+  Serial.print("Connect with MQTT broker");
+  while(!client.connect(SERIAL_NUMBER)) {
+    Serial.print(".");
+    delay(500);
+  }
 }
 
 void configureWiFi() {
@@ -44,14 +75,21 @@ void setup() {
   Serial.println();
 
   configureWiFi();
+  delay(100);
+
+  client.begin("192.168.8.185", 1883, net);
 }
 
 void loop() {
-  readData();
+  if(!client.connected()) {
+    reconnect();
+  }
 
-  Serial.println("temp = " + String(temperature));
-  Serial.println("hum = " + String(humidity));
-  Serial.println("pres = " + String(pressure));
-  Serial.println("alt = " + String(altitude));
+  readData();
+  generateJson();
+
+  client.loop();
+
+  sendMessage();
   delay(10000);
 }
